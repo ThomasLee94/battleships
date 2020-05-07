b83e00da1e71c192f4109b84d972e70b43353f89
ThomasLee94 <thomaslee9474@gmail.com>
2020-05-07 15:06:41 -0700

    [fix] script

--- scripts/generate_stubs.sh
+++ scripts/generate_stubs.sh
@@ -1,9 +1,8 @@
 ROOT_DIR=$(git rev-parse --show-toplevel)
-DIR="$ROOT_DIR/server/proto"
-OUT_DIR="$ROOT_DIR/client/src/generated/src"
+DIR="$ROOT_DIR/battleships/src/battleship_service/proto"
+OUT_DIR="$ROOT_DIR/src/generated/src"
 
 rm -r "$OUT_DIR"
 mkdir -p "$OUT_DIR"
 
-protoc-gen-grpc --proto_path="$DIR" echo.proto --js_out=import_style=commonjs:$OUT_DIR --grpc-web_out=import_style=commonjs,mode=grpcwebtext:$OUT_DIR
-#protoc-gen-grpc --proto_path="$DIR" echo.proto --grpc-web_out=mode=grpcwebtext:$OUT_DIR
+protoc-gen-grpc --proto_path="$DIR" service.proto --js_out=import_style=commonjs:$OUT_DIR --grpc-web_out=import_style=commonjs,mode=grpcwebtext:$OUT_DIR
