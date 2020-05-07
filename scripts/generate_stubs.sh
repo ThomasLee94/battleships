cee836536eeaf1ea3d8cd04af9aea8a13e2f2733
ThomasLee94 <thomaslee9474@gmail.com>
2020-05-07 15:11:12 -0700

    [add] services stubs

--- scripts/generate_stubs.sh
+++ scripts/generate_stubs.sh
@@ -5,4 +5,4 @@ OUT_DIR="$ROOT_DIR/src/generated/src"
 rm -r "$OUT_DIR"
 mkdir -p "$OUT_DIR"
 
-protoc-gen-grpc --proto_path="$DIR" service.proto --js_out=import_style=commonjs:$OUT_DIR --grpc-web_out=import_style=commonjs,mode=grpcwebtext:$OUT_DIR
+protoc-gen-grpc --proto_path="$DIR" services.proto --js_out=import_style=commonjs:$OUT_DIR --grpc-web_out=import_style=commonjs,mode=grpcwebtext:$OUT_DIR
