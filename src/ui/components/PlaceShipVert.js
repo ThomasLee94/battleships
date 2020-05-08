42f1d73efe20544a2b7441c7019e72d8ddb1f8d8
ThomasLee94 <thomaslee9474@gmail.com>
2020-05-07 23:35:50 -0700

    [add] state for place ship vert

--- src/ui/components/PlaceShipVert.js
+++ src/ui/components/PlaceShipVert.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import './PlaceShipVert.css'
 
-function PlaceShipVert(props) {
+class PlaceShipVert extends React.Component {
+    constructor(props) {
+        super(props);
+          this.state = {
+            row: 1,
+            row_start: 1,
+            row_end: 1,
+            col: 2,
+            col_start: 2,
+            col_end:2
+          }
+        }
+   
 
-    const {type} = props;
-
-    return (
+    return() {
         <form className="PlaceShipVert">
             <label >
                 row start
@@ -22,7 +32,7 @@ function PlaceShipVert(props) {
             {/* <button onClick={activateLasers}> Activate Lasers</button> */}
             <button className="button"> Submit</button>
         </form>
-    )
+    }
 }
 
 export default PlaceShipVert
