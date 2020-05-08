eb68b370bccec4e54f64381c45ffe7ad3268ab83
ThomasLee94 <thomaslee9474@gmail.com>
2020-05-07 23:47:36 -0700

    [add] on click

--- src/ui/components/PlaceShipVert.js
+++ src/ui/components/PlaceShipVert.js
@@ -5,14 +5,22 @@ class PlaceShipVert extends React.Component {
     constructor(props) {
         super(props);
           this.state = {
-            row: 1,
-            row_start: 1,
-            row_end: 1,
-            col: 2,
-            col_start: 2,
-            col_end:2
-          }
+            row: 0,
+            row_start: 0,
+            row_end: 0,
+            col: 0,
+            col_start: 0,
+            col_end: 0
+          };
+
+          this.submitClick = this.submitClick.bind(this)
         }
+
+    submitClick() {
+        this.setState(state => ({
+            // what to do?
+        }))
+    }
    
 
     return() {
@@ -29,8 +37,7 @@ class PlaceShipVert extends React.Component {
                 col
                 <input type="text" name="row-start"/>
             </label>
-            {/* <button onClick={activateLasers}> Activate Lasers</button> */}
-            <button className="button"> Submit</button>
+            <button onClick={() => this.submitClick()} className="button"> Submit</button>
         </form>
     }
 }
